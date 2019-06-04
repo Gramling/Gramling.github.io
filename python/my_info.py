ac864e828cb83fa4c7ecaa19217d0e2ba7bf11e8
Gramling <v.gramling@miami.edu>
2019-06-04 11:18:05 -0400

    Updated file

--- python/my_info.py
+++ python/my_info.py
@@ -1,6 +1,1 @@
-my_info = {
-"first_name": "Valerie",
-"last_name": "Gramling",
-"hometown": "Coral Gables",
-"favorite_food": "popcorn"
-}
+my_info = {"first_name": "Valerie", "last_name": "Gramling", "hometown": "Coral Gables", "favorite_food": "popcorn"}
