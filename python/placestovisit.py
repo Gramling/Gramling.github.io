d7137eac6bc5cf14505f06508ce1e5ecad3a7c9d
Gramling <v.gramling@miami.edu>
2019-06-04 16:11:30 -0400

    New comprehension exercise

---
+++ python/placestovisit.py
@@ -0,0 +1,5 @@
+places = ["Norway", "Montenegro", "St. Davids", "Copenhagen"]
+places
+['Norway', 'Montenegro', 'St. Davids', 'Copenhagen']
+for place in places:
+    print("I would like to visit " + place)
