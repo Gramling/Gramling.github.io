8ca9b3e0abb8163f6b1a32f1f06a0186d507716b
Gramling <v.gramling@miami.edu>
2019-06-04 15:02:20 -0400

    Fixed problem in dictionary

--- python/returnaddress2.py
+++ python/returnaddress2.py
@@ -1,4 +1,4 @@
-return_address = {"Name": "Valerie Gramling", "Street": "5801 Marius Street", "City": "Coral Gables," "State": "FL", "Zip": "33146"}
+return_address = {"Name": "Valerie Gramling", "Street": "Broadway", "City": "Naboo,", "State": "USA", "Zip": "55555"}
 print(return_address["Name"])
 print(return_address["Street"])
 print(return_address["City"] + return_address["State"] + ' ' + return_address["Zip"])
