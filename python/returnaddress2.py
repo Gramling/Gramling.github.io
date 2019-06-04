ca4183461307c36c55846f5a45fc3bd4177049ca
Gramling <v.gramling@miami.edu>
2019-06-04 14:30:56 -0400

    return address file in dictionary format

---
+++ python/returnaddress2.py
@@ -0,0 +1,4 @@
+return_address = {"Name": "Valerie Gramling", "Street": "5801 Marius Street", "City": "Coral Gables," "State": " FL", "Zip": " 33146"}
+print(return_address["Name"])
+print(return_address["Street"])
+print(return_address["City"] + return_address["State"] + return_address[" Zip"])
