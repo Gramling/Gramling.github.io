aadfca7d5bbcdfd6ec41295d8d9d0a314b3fc8a0
Gramling <v.gramling@miami.edu>
2019-06-04 16:16:21 -0400

    added period at end of sentence

--- python/placestovisit.py
+++ python/placestovisit.py
@@ -2,4 +2,4 @@ places = ["Norway", "Montenegro", "St. Davids", "Copenhagen"]
 places
 ['Norway', 'Montenegro', 'St. Davids', 'Copenhagen']
 for place in places:
-    print("I would like to visit " + place)
+    print("I would like to visit " + place + ".")
