d0a295fe7f68442f7f6ddcb4013dc1621807becf
Gramling <v.gramling@miami.edu>
2019-06-05 11:23:24 -0400

    Created new file

---
+++ python/bookdatepractice.py
@@ -0,0 +1,6 @@
+#Create dictionary to store collection of titles and dates
+Books = {"Jane Eyre": "1847", "Cane": "1923", "Wide Sargasso Sea": "1966", "Citizen: An American Lyric": "2014"}
+#Create a for loop to print out all data in specific format
+for title, date in Books.items():
+    print(title + " was published in " + date + ".")
+#Figure out method for automating process
---
+++ python/methodpractice2.py
@@ -0,0 +1,13 @@
+def hello(parrot):
+    print("This parrot" + parrot)
+
+
+
+
+parrot = " is an ex-parrot"
+
+def deadparrot(name):
+    print("This is the " + name)
+    hello(parrot)
+
+deadparrot("Dead Parrot Sketch")
