abe6a4b34eb62d8502a02394ac7bd122bbbe2249
Gramling <v.gramling@miami.edu>
2019-06-05 10:10:25 -0400

    Created file to practice methods

---
+++ python/methodpractice.py
@@ -0,0 +1,14 @@
+def hello(parrot):
+    print("This is an ex-parrot" + parrot)
+
+
+
+
+parrots = ["-bereft of life", "-joined the choir invisible"]
+
+for parrot in parrots:
+    hello(parrot)
+
+
+
+    
