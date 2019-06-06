0aeece139eac4eb7b83541e554725e35b3878ea7
Gramling <v.gramling@miami.edu>
2019-06-05 20:45:11 -0400

    debugged conditionals program

--- python/scrape.py
+++ python/scrape.py
@@ -21,4 +21,28 @@ links = soup.find_all("a")[0:10]
 links_html = soup.select("td.content a")
 this_link = links_html[0]
 
-print(this_link["href"])
+urls = []
+#take each link and make a new list w/processed URLs
+for link in links_html:
+    to_append = link["href"].replace("blob/", "")
+    urls.append("https://raw.githubusercontent.com" + to_append)
+
+test_url = urls[3]
+corpus_texts = []
+
+for url in urls:
+    html = request.urlopen(url).read()
+    soup = BeautifulSoup(html, 'lxml')
+    text = soup.text.replace("\n", "")
+    corpus_texts.append(text)
+    print("Scraping " + url)
+
+print(len(corpus_texts))
+print(len(corpus_texts[0]))
+
+this_text = corpus_texts[0]
+process_this_text =  nltk.word_tokenize(this_text)
+print(process_this_text[0:20])
+print(nltk.FreqDist(process_this_text).most_common(50))
+
+ 
